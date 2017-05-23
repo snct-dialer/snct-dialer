4df5b8e088576e736b7a591b3328485863dad2a4
Jörg Frings-Fürst <debian@jff-webhosting.net>
2017-05-23 12:17:21 +0200

    Finish for release

--- FlyInclude.php
+++ FlyInclude.php
@@ -6,9 +6,10 @@
 # 2017-003 JFF 2017-05-22 Set consultative_checkbox always checked
 # 2017-004 JFF 2017-05-22 Add Mute marker after Status at live_report
 # 2017-005 JFF 2017-05-23 Display git_[commit|release} at System Settings
+# 2017-006 JFF 2017-05-23 Some typos and missing parts
 #
 
-$FLY_patch_level = "2017-005";
+$FLY_patch_level = "2017-006";
 
 
 ?>
