a76bc4f06f47587a72ef2e9f62a97aa5fdeacd1e
Jörg Frings-Fürst <debian@jff-webhosting.net>
2017-06-08 18:31:57 +0200

    manual merge

--- FlyInclude.php
+++ FlyInclude.php
@@ -12,8 +12,8 @@
 #          JFF 2017-05-27 Add svn commit 2759
 #          JFF 2017-05-27 Add svn commit 2760
 #          JFF 2017-05-28 Add svn commit 2761
-#
 #          JFF 2017-06-07 New "copy status groups"
+#
 
 $FLY_patch_level = "2017-008";
 
