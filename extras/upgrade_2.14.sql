7ba3736a379b0493ec62cf5a2545aaee586feebd
Jörg Frings-Fürst <debian@jff-webhosting.net>
2017-10-08 13:47:43 +0200

    Add upstream Rev. 2836

--- extras/upgrade_2.14.sql
+++ extras/upgrade_2.14.sql
@@ -442,3 +442,7 @@ ALTER TABLE server_performance ADD disk_reads MEDIUMINT(7);
 ALTER TABLE server_performance ADD disk_writes MEDIUMINT(7);
 
 UPDATE system_settings SET db_schema_version='1520',db_schema_update_date=NOW() where db_schema_version < 1520;
+
+ALTER TABLE vicidial_lists ADD inbound_list_script_override VARCHAR(20);
+
+UPDATE system_settings SET db_schema_version='1521',db_schema_update_date=NOW() where db_schema_version < 1521;
