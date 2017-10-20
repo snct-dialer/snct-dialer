aa67c3668722ee799af0ce0769af95d89d6ca61a
Jörg Frings-Fürst <debian@jff-webhosting.net>
2017-10-20 17:22:06 +0200

    Wallboard: Add new table WallBoardStat

--- extras/MySQL_AST_CREATE_tables.sql
+++ extras/MySQL_AST_CREATE_tables.sql
@@ -3838,6 +3838,47 @@ index(phone_number),
 index(scrub_date)
 ) ENGINE=MyISAM;
 
+CREATE TABLE IF NOT EXISTS `WallBoardStat` (
+  `ID` bigint(20) NOT NULL,
+  `StandVom` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Datum` date DEFAULT NULL,
+  `Gruppe` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Kunden` bigint(20) DEFAULT '0',
+  `Calls` bigint(20) DEFAULT '0',
+  `phones` bigint(20) DEFAULT '0',
+  `Drops` bigint(20) DEFAULT '0',
+  `WL` bigint(20) DEFAULT '0',
+  `AB` bigint(20) DEFAULT '0',
+  `Agent` bigint(20) DEFAULT '0',
+  `ebk` double DEFAULT '0',
+  `kebk` double DEFAULT '0',
+  `SL0` bigint(20) DEFAULT '0',
+  `SL1` bigint(20) DEFAULT '0',
+  `SL2` bigint(20) DEFAULT '0',
+  `waittime` double DEFAULT '0',
+  `Unbekannt` bigint(20) DEFAULT '0',
+  `TKunden` bigint(20) NOT NULL DEFAULT '0',
+  `TCalls` bigint(20) NOT NULL DEFAULT '0',
+  `Tphones` bigint(20) NOT NULL DEFAULT '0',
+  `TDrops` bigint(20) NOT NULL DEFAULT '0',
+  `TWL` bigint(20) NOT NULL DEFAULT '0',
+  `TAB` bigint(20) NOT NULL DEFAULT '0',
+  `TAgent` bigint(20) NOT NULL DEFAULT '0',
+  `Tebk` double NOT NULL DEFAULT '0',
+  `Tkebk` double NOT NULL DEFAULT '0',
+  `TSL0` bigint(20) NOT NULL DEFAULT '0',
+  `TSL1` bigint(20) NOT NULL DEFAULT '0',
+  `TSL2` bigint(20) NOT NULL DEFAULT '0',
+  `Twaittime` double NOT NULL DEFAULT '0',
+  `TUnbekannt` bigint(20) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `WallBoardStat`
+  ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `WallBoardStat`
+  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
 
 ALTER TABLE vicidial_email_list MODIFY message text character set utf8;
 
