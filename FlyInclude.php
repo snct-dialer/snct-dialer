69d99c55534af0f485e712b077e086afc00e5f7d
Jörg Frings-Fürst <debian@jff.email>
2018-02-18 13:48:56 +0100

    Release 2.25.24

--- FlyInclude.php
+++ FlyInclude.php
@@ -1,51 +1,7 @@
 <?php
 
-#
-# 2017-001 JFF 2017-05-18 Add Patchlevel and modify copyright
-# 2017-002 JFF 2017-05-18 replace call_date with closecallid at UPDATE vicidial_closer_log
-# 2017-003 JFF 2017-05-22 Set consultative_checkbox always checked
-# 2017-004 JFF 2017-05-22 Add Mute marker after Status at live_report
-# 2017-005 JFF 2017-05-23 Display git_[commit|release} at System Settings
-# 2017-006 JFF 2017-05-23 Some typos and missing parts
-# 2017-007 JFF 2017-05-24 Cherrypick to svn 2757
-# 2017-008 JFF 2017-05-27 Add svn commit 2758
-# 2017-008 JFF 2017-05-27 Add svn commit 2759
-# 2017-008 JFF 2017-05-27 Add svn commit 2760
-# 2017-008 JFF 2017-05-28 Add svn commit 2761
-#          JFF 2017-05-31 Add svn commit 2762
-#          JFF 2017-05-31 Add svn commit 2763
-#          JFF 2017-05-31 Add svn commit 2764
-#          JFF 2017-05-31 Add svn commit 2765
-#          JFF 2017-05-31 Add svn commit 2766
-#          JFF 2017-05-31 Add svn commit 2767
-#          JFF 2017-06-05 Add svn commit 2768
-#          JFF 2017-06-05 Add svn commit 2769
-#          JFF 2017-06-05 Add svn commit 2770
-#          JFF 2017-06-05 Add svn commit 2771
-#          JFF 2017-06-05 Add svn commit 2772
-#          JFF 2017-06-05 Add svn commit 2773
-#          JFF 2017-06-07 New "copy status groups"
-# 2017-009 JFF 2017-06-08 New "callback caledar with holiday colors"
-#          JFF 2017-06-10 Add svn commit 2774-2777
-# 2017-010 JFF 2017-06-10 Add svn commit 2778
-#          JFF 2017-06-15 Add field 'Campaign Pause' into system_settings
-#          JFF 2017-06-16 Add svn commits 2779-2780
-# 2017-011 JFF 2017-06-16 Finish 'Campaign Pause'
-# 2017-012 JFF 2017-06-17 Add missing ingroup selection
-#          JFF 2017-06-21 Change ulaw to alaw for interconnections
-#          JFF 2017-06-21 Display only active callbacks
-# 2017-013 JFF 2017-06-26 Add svn commits 2781-2782
-# 2017-014 JFF 2017-06-26 Merge
-#          JFF 2017-06-30 Add svn commit 2783-2784
-# 2017-015 JFF 2017-06-30 Add Company Name to Title
-#          JFF 2017-06-30 Add detect 3Way
-# 2017-016 JFF 2017-06-30 Add svn commit 2785
-#          JFF 2017-07-04 Add ExtraMenueReports
-# 2017-017 JFF 2017-07-08 Add svn commit 2786
-#
-
-$FLY_patch_level = "2017-062";
-$FLY_SVN_base = "2880";
-$FLY_version  = "2.25.23";
+$FLY_patch_level = "2018-002";
+$FLY_SVN_base = "2923";
+$FLY_version  = "2.25.24";
 
 ?>
