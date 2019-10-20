7acb0bdaeb861cee4cd4623a3a16f23cbb82492f
Jörg Frings-Fürst <open-source@jff.email>
2019-10-20 12:49:56 +0200

    Add upstream Rev. 3145

--- www/agc/vdc_script_display.php
+++ www/agc/vdc_script_display.php
@@ -3,7 +3,7 @@
 # 
 # LICENSE: AGPLv3
 #
-# Copyright (©) 2018  Matt Florell <vicidial@gmail.com>
+# Copyright (©) 2019  Matt Florell <vicidial@gmail.com>
 # Copyright (©) 2019      SNCT GmbH <info@snct-gmbh.de>
 #               2017-2019 Jörg Frings-Fürst <open_source@jff.email>.
 #
@@ -43,6 +43,7 @@
 # 171126-1124 - Added email message display from inbound emails only
 # 180224-1406 - Added LOGINvar variables, and options.php $INSERT_ variables
 # 180327-1356 - Added code for LOCALFQDN conversion to browser-used server URL for script iframes
+# 191013-2145 - Fixes for PHP7
 #
 
 # Other - Changelog
@@ -51,8 +52,8 @@
 # 2019-04-29 10:08 Add system_wide_settings.php
 #
 
-$version = '2.14-33';
-$build = '180224-1406';
+$version = '2.14-34';
+$build = '191013-2145';
 
 require_once("dbconnect_mysqli.php");
 require_once("functions.php");
@@ -765,6 +766,23 @@ if (preg_match('/--A--TABLEper_call_notes--B--/i',$script_text))
 		$out_logs_to_print = mysqli_num_rows($rslt);
 		if ($format=='debug') {$NOTESout .= "|$out_logs_to_print|$stmt|";}
 
+		$ALLsort = array();
+		$ALLstart_epoch = array();
+		$ALLcall_date = array();
+		$ALLcampaign_id = array();
+		$ALLlength_in_sec = array();
+		$ALLstatus = array();
+		$ALLphone_code = array();
+		$ALLphone_number = array();
+		$ALLlead_id = array();
+		$ALLhangup_reason = array();
+		$ALLalt_dial = array();
+		$ALLuniqueid = array();
+		$ALLuser = array();
+		$ALLin_out = array();
+		$Allcall_notes = array();
+		$ALLemail = array();
+		$Allcounter = array();
 		$g=0;
 		$u=0;
 		while ($out_logs_to_print > $u) 
